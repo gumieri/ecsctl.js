f6fbbf5668e51a4700e407bf172c7eedd0ac986f
Rafael Gumieri <rafael.gumieri@tecnospeed.com.br>
2018-03-02 14:37:52 -0300

    New command: copy-services between clusters
    The command interface changed:
    - `ecsedit task-definition` is the original command;
    - `ecsedit copy-services` is the command introduced in this version.

--- index.js
+++ index.js
@@ -1,60 +1,26 @@
-const AWS = require('aws-sdk')
-const flags = require('commander')
+const commander = require('commander')
 
-const editor = require('./lib/editor')
+const package = require('./package')
 
-flags
-  .version('0.0.1')
-  .option('--task-definition [name]', 'Task Definition name')
-  .option('--service [name]', 'Service name')
+// commands
+const edit = require('./commands/edit')
+const copyServices = require('./commands/copy_services')
+
+commander.version(package.version)
+
+commander
+  .command('task-definition <name>')
+  .alias('td')
   .option('--editor [name]', 'Editor name or command to execute')
   .option('--deploy', 'To update and deploy the changes')
-  .parse(process.argv)
+  .action(edit)
 
-const fatal = err => {
-  console.log(err)
-  return process.exit(1)
-}
+commander
+  .command('copy-services <service...>')
+  .alias('cps')
+  .option('--deploy', 'To update and deploy the changes')
+  .option('--cluster <name>', 'Cluster name')
+  .option('--to-cluster <name>', 'Cluster name to be copied the service')
+  .action(copyServices)
 
-const ecs = new AWS.ECS({ apiVersion: '2014-11-13' })
-ecs.describeTaskDefinition({ taskDefinition: flags.taskDefinition }, (err, data) => {
-  if (err) return fatal(err)
-
-  let currentRevison = data.taskDefinition.revision
-  let dataToBeEdited = Object.assign({}, data.taskDefinition)
-
-  delete dataToBeEdited.requiresAttributes
-  delete dataToBeEdited.taskDefinitionArn
-  delete dataToBeEdited.compatibilities
-  delete dataToBeEdited.revision
-  delete dataToBeEdited.status
-
-  let editorParams = {
-    editor: flags.editor || process.env.EDITOR,
-    filename: `${flags.taskDefinition}-task-definition-`,
-    content: JSON.stringify(dataToBeEdited, null, 2),
-    extension: 'json'
-  }
-
-  editor.edit(editorParams, (err, editedBuffer) => {
-    if (err) return fatal(err)
-
-    if (editedBuffer == false) {
-      console.log('Empty entry')
-      return process.exit(1)
-    }
-
-    let edited = JSON.parse(editedBuffer)
-
-    if (!flags.deploy) return process.exit()
-
-    ecs.registerTaskDefinition(edited, err => {
-      if (err) return fatal(err)
-
-      ecs.deregisterTaskDefinition({ taskDefinition: `${escedit.taskDefinition}:${currentRevison}` }, err => {
-        if (err) return fatal(err)
-        return process.exit()
-      })
-    })
-  })
-})
+commander.parse(process.argv)
